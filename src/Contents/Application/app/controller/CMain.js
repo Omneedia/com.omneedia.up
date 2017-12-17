dce0c80e52bc6590962d2e2f46504e93492e15e6
Stéphane Zucatti <stephane.zucatti@me.com>
2017-12-17 19:08:17 +0100

    small fixes

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -20,15 +20,34 @@ App.controller.define('CMain', {
 
     },
     "load-click": function(event) {
-        App.$(".load").hide();
-        cordova.plugins.barcodeScanner.scan(
+        var diont = Diont();
+
+        // ====== 
+        // Listen for announcements and renouncements in services 
+        // ====== 
+        diont.on("serviceAnnounced", function(serviceInfo) {
+            // A service was announced 
+            // This function triggers for services not yet available in diont.getServiceInfos() 
+            // serviceInfo is an Object { isOurService : Boolean, service: Object } 
+            // service.name, service.host and service.port are always filled 
+            console.log("A new service was announced", serviceInfo.service);
+            // List currently known services 
+            console.log("All known services", diont.getServiceInfos());
+        });
+
+        diont.on("serviceRenounced", function(serviceInfo) {
+            console.log("A service was renounced", serviceInfo.service);
+            console.log("All known services", diont.getServiceInfos());
+        });
+        //App.$(".load").hide();
+        /*cordova.plugins.barcodeScanner.scan(
             function(result) {
                 window.location.href = result.text;
             },
             function(error) {
                 alert("Scanning failed: " + error);
             }
-        );
+        );*/
     }
 
 
