cb7290f92c6e077231aff2f4bf31fa8a6abf99e0
Stéphane Zucatti <stephane.zucatti@me.com>
2018-01-04 08:26:53 +0100

    ???

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -21,6 +21,24 @@ App.controller.define('CMain', {
     },
     "load-click": function(event) {
 
+        function createFile() {
+            var type = window.TEMPORARY;
+            var size = 5 * 1024 * 1024;
+            window.requestFileSystem(type, size, successCallback, errorCallback)
+
+            function successCallback(fs) {
+                fs.root.getFile('log.txt', { create: true, exclusive: true }, function(fileEntry) {
+                    alert('File creation successfull!')
+                }, errorCallback);
+            }
+
+            function errorCallback(error) {
+                alert("ERROR: " + error.code)
+            }
+
+        };
+
+        createFile();
 
         App.$(".load").hide();
         cordova.plugins.barcodeScanner.scan(
